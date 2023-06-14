bef85df471055945b2ba383dc13e5102d8ea1dd7
bab3642 <bbperc1@gmail.com>
2023-06-14 18:40:01 +0000

    add lesson 1 and some lesson 2 exercises

---
+++ RB101/lesson-1/practice.rb
@@ -0,0 +1,9 @@
+NUMBERS = [1, 2, 3]
+arr = [2, 3, 4]
+
+def test
+  puts NUMBERS.inspect
+  puts arr.inspect
+end
+
+test
---
+++ RB101/lesson_2/practice.rb
@@ -0,0 +1,8 @@
+num = 5
+
+if (num < 10)
+  puts "small"
+else
+  puts "large"
+end
+
