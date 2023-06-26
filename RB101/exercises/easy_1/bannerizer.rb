3ae0c2b8aa9bce2637f2ab787f902540ce8dd37c
bab3642 <bbperc1@gmail.com>
2023-06-26 20:40:00 +0000

    complete further explorations of bannerizer.rb

--- RB101/exercises/easy_1/bannerizer.rb
+++ RB101/exercises/easy_1/bannerizer.rb
@@ -1,7 +1,30 @@
+require 'pry'
+
 def print_in_box(text = "")
-  if text.size > 80
+  if text.size > 76
+    minuses = ""
+    spaces = ""
+    76.times do
+      minuses << "-"
+      spaces << " "
+    end
+    puts "+-#{minuses}-+"
+    puts "| #{spaces} |"
+    
     loop do
-      text.split()
+      chopped = text.slice!(0...76)
+      break if chopped == ""
+      if chopped.size < 75
+        extra_spaces = ""
+          (76 - chopped.size).times { extra_spaces << " " }
+          puts "| " + chopped + extra_spaces + " |"
+      else
+        puts "| " + chopped + " |"
+      end
+    end
+    
+    puts "| #{spaces} |"
+    puts "+-#{minuses}-+"
   else
     minuses = ""
     spaces = ""
@@ -22,3 +45,5 @@ print_in_box('To boldly go where no one has gone before.')
 print_in_box()
 print_in_box("I am having fun!")
 print_in_box("There is probably a better way to do this!")
+print_in_box("THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! THIS IS MORE THAN 80 CHARACTERS! ")
+print_in_box("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
