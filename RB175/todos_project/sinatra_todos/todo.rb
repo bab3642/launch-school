ceb9808077c3038dbfc22d8341f02b1ea812950f
bab3642 <bbperc1@gmail.com>
2023-11-13 16:41:01 +0000

    completing all items on todo list lesson

--- RB175/todos_project/sinatra_todos/todo.rb
+++ RB175/todos_project/sinatra_todos/todo.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -18,7 +19,7 @@ end
 # View list of lists
 get "/lists" do
   @lists = session[:lists]
-  erb :list, layout: :layout
+  erb :lists, layout: :layout
 end
 
 # Render the new list form
@@ -34,6 +35,12 @@ def error_for_list_name(name)
   end
 end
 
+def error_for_todo(name)
+  if !(1..100).cover?(name.size)
+    "Todo must be between 1 - 100 characters."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
@@ -46,13 +53,83 @@ post "/lists" do
     session[:success] = "The list has been created."
     redirect "/lists"
   end
-
-
-
 end
 
-get "/lists/:number" do
+# update an existing todo list
+post "/lists/:id" do
+  id = params[:id].to_i
+  list_name = params[:list_name].strip
+  @list = session[:lists][id]
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
 
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  @id = params[:id]
+  @list = session[:lists][@id.to_i]
+  erb :edit_list, layout: :layout
+end
+
+get "/lists/:id" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :list
+end
+
+# Delete an existing list
+post "/lists/:id/destroy" do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+end
+
+# Add a todo item to an existing list
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << { name: text, completed: false }
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Delete a todo item from a list
+post "/lists/:list_id/todos/:todo_id/destroy" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:todo_id].to_i
+  @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
+
+# Update status of a todo item
+post "/lists/:list_id/todos/:todo_id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:todo_id].to_i
+  is_completed = (params[:completed] == "true")
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
+  redirect "/lists/#{@list_id}"
 end
 
 set :session_secret, SecureRandom.hex(32)
